62e922735bcde98df5fa7392970ba7c9c3129b6d
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-08-05 15:18:20 +0400

    v1.08 Added WirkSlider page, components, store.

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import globalReducer from 'features/global/GlobalSlice'
+import mainReducer from 'features/main/mainSlice'
 
 export const store = configureStore({
   reducer: {
+    global: globalReducer,
+    main: mainReducer,
     counter: counterReducer,
   },
 });
--- frontend/src/pages/Main/content/WorkSlider/WorkSlider.jsx
+++ frontend/src/pages/Main/content/WorkSlider/WorkSlider.jsx
@@ -1,23 +1,23 @@
-import { Box } from '@mui/material';
-import { memo } from 'react'
-import styled from 'styled-components';
+import { Box } from "@mui/material";
+import { memo } from "react";
+import WorkSliderComponent from "components/WorkSliderComponent";
+
+import styled from "styled-components";
 
 function WorkSlider() {
-    return(
-        <MainContainer>
-            WorkSlider
-        </MainContainer>
-    )
+  return (
+    <MainContainer>
+      <WorkSliderComponent></WorkSliderComponent>
+    </MainContainer>
+  );
 }
 
-export default memo(WorkSlider)
+export default memo(WorkSlider);
 
-const MainContainer = styled(Box)(({ theme }) => ({
-    maxWidth: "100%",
-    minHeight: "650px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: theme.palette.background.action
-  }));
+const MainContainer = styled(Box)(({ _ }) => ({
+  maxWidth: "100%",
+  minHeight: "650px",
+  paddingBottom: "30px",
+  display: "flex",
+  justifyContent: "center",
+}));
